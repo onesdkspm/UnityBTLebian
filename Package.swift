51f037acbb2d9f1407f54e3ab18fe348542c819a
m2构造机 <zhanggaoqi@aobi.com>
2026-07-08 17:52:11 +0800

    [Branch Release] v2.0.3-dev-1481412 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1481412
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "UnityBTLebian",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTLebian/2.0.2-dev-1481110/UnityBTLebian.xcframework.zip",
-            checksum: "a14cce5998bfbaee75c9b9decfe85e8b5fb324ff07f65c8a0eb2b9f0252a36ec"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBTLebian/2.0.3-dev-1481412/UnityBTLebian.xcframework.zip",
+            checksum: "694ce373e9a560b7aaa508aec8805640b9d195490d6d433f96106b63251774e8"
         )
     ]
 )
